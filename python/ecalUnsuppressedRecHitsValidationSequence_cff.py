eaba039a422fa79868e06412f31e2160895cb9c2
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2012-06-06 20:20:47 +0000

    back out change made obsolete by EDAlias feature

--- python/ecalUnsuppressedRecHitsValidationSequence_cff.py
+++ python/ecalUnsuppressedRecHitsValidationSequence_cff.py
@@ -6,7 +6,7 @@ from Validation.EcalRecHits.ecalBarrelRecHitsValidation_cfi import *
 from Validation.EcalRecHits.ecalEndcapRecHitsValidation_cfi import *
 from Validation.EcalRecHits.ecalPreshowerRecHitsValidation_cfi import *
 ecalUnsuppressedRecHitsValidationSequence = cms.Sequence(ecalRecHitsValidation*ecalBarrelRecHitsValidation*ecalEndcapRecHitsValidation*ecalPreshowerRecHitsValidation)
-ecalBarrelRecHitsValidation.EBdigiCollection = cms.InputTag('mix', 'simEcalUnsuppressedDigis')
-ecalEndcapRecHitsValidation.EEdigiCollection = cms.InputTag('mix', 'simEcalUnsuppressedDigis')
+ecalBarrelRecHitsValidation.EBdigiCollection = 'simEcalUnsuppressedDigis'
+ecalEndcapRecHitsValidation.EEdigiCollection = 'simEcalUnsuppressedDigis'
 
 
